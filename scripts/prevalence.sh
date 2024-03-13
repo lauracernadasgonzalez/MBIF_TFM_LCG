b7e05efcec8bdae54803e51c231da0a1b5b08330
lauracernadasgonzalez <162823985+lauracernadasgonzalez@users.noreply.github.com>
2024-03-13 20:38:35 +0100

    Delete scripts directory

--- scripts/prevalence.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-nuevo_directorio="/home/laura.cernadas/00TFM/datos/reads/variant_calling/archivos_variantes_parental/variantes_tabla"
-cd $nuevo_directorio
-
-for archivo in "$nuevo_directorio"/*.sorted; do
-	head -n 1 "$archivo" > "${archivo}_prevalence"
-	awk 'NR>1' $archivo | sort -nr -k 7 >> "${archivo}_prevalence"
-done
